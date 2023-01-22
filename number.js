21a7c64cc7e6d0e7684f127486917b1b99a2e20d
Madison Evans <m.corbinevans@gmail.com>
2023-01-22 13:08:13 -0700

    number.js added to main branch

---
+++ number.js
@@ -0,0 +1,39 @@
+const resultDiv = document.querySelector("#trivia");
+
+//helper methods
+function queryDb(number) {
+	getJSON(`http://numbersapi.com/${number}?json`).then(renderResult);
+	getJSON(`http://numbersapi.com/${number}/year?json`).then(renderResult);
+	getJSON(`http://numbersapi.com/${number}/math?json`).then(renderResult);
+}
+
+function getJSON(url) {
+	return fetch(url)
+		.then((response) => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw "Error fetching resources. Status code: " + response.status;
+			}
+		})
+		.catch((errorMsg) => console.log(errorMsg));
+}
+
+function renderResult(data) {
+	console.log(data);
+	insertResultsHeaderIfNotPresent();
+	if (data.found) {
+		const p = document.createElement("p");
+		p.className = "result";
+		p.textContent = data.text;
+		resultDiv.append(p);
+	}
+}
+
+function insertResultsHeaderIfNotPresent() {
+	if (!resultDiv.contains(resultDiv.querySelector("h2"))) {
+		const heading = document.createElement("h2");
+		heading.textContent = "Results";
+		resultDiv.append(heading);
+	}
+}
